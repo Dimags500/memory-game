6abb54f16e8a0188fb09285565acbc64b22c34f8
‏‏‏‏Matan‏ ‏‏‏‏ ‏‏ ‏ <drms12@gmail.com>
2022-05-08 13:19:40 +0300

    Main game functionality

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
-const landingBtn = document.querySelector("button");
 const landingPage = document.getElementById("landing-page");
+const landingBtn = document.querySelector("button");
 const container = document.querySelector(".game-container");
+const moveCounter = document.querySelector(".moves");
+const timer = document.querySelector(".timer");
 const gameBoard = document.querySelector(".game-board");
+const won = document.getElementById("overlay");
+const wonContent = document.getElementById("won-page");
+const wonBtn = document.querySelector(".won-page__btn");
+const moveCounterEnd = document.querySelector(".moves-end");
+const timerEnd = document.querySelector(".timer-end");
+const themeButton = document.getElementById("theme");
 const radioButtons = document.querySelectorAll('input[name="difficulty"]');
 
 const gameObj = {
@@ -30,8 +38,28 @@ landingBtn.addEventListener("click", () => {
       break;
     }
   }
-
   drawBoard();
+});
+
+themeButton.addEventListener("click", (e) => {
+  const cardFront = document.querySelectorAll(".card-front");
+  if (e.target.textContent === "Dark Mode") {
+    for (let i = 0; i < cardFront.length; i++) {
+      cardFront[i].classList.add("card-front__dark");
+    }
+    container.classList.add("game-container__dark");
+    moveCounter.style.color = "white";
+    timer.style.color = "white";
+    e.target.textContent = "Light Mode";
+  } else {
+    for (let i = 0; i < cardFront.length; i++) {
+      cardFront[i].classList.remove("card-front__dark");
+    }
+    container.classList.remove("game-container__dark");
+    moveCounter.style.color = "black";
+    timer.style.color = "black";
+    e.target.textContent = "Dark Mode";
+  }
 });
 
 function getRandomInt(min, max) {
@@ -88,4 +116,43 @@ const drawBoard = () => {
     cardElement.appendChild(cardBack);
     gameBoard.appendChild(cardElement);
   }
+};
+
+gameBoard.addEventListener("click", (e) => {
+  const eventTarget = e.target;
+  const eventParent = eventTarget.parentElement;
+  if (
+    eventTarget.className.includes("card") &&
+    !eventParent.className.includes("flipped")
+  ) {
+    flipCard(eventParent);
+  }
+});
+
+const flipCard = (card) => {
+  gameObj.flippedCards++;
+  if (gameObj.flippedCards <= 2) {
+    gameObj.totalFlips++;
+    card.classList.add("flipped");
+  }
+
+  if (gameObj.flippedCards === 2) {
+    const flippedCards = document.querySelectorAll(".flipped:not(.matched)");
+    if (
+      flippedCards[0].childNodes[1].classList[1] ===
+      flippedCards[1].childNodes[1].classList[1]
+    ) {
+      flippedCards[0].classList.add("matched");
+      flippedCards[1].classList.add("matched");
+    }
+    setTimeout(() => {
+      flipBackCards();
+    }, 1000);
+  }
+};
+const flipBackCards = () => {
+  document.querySelectorAll(".card:not(.matched)").forEach((card) => {
+    card.classList.remove("flipped");
+  });
+  gameObj.flippedCards = 0;
 };
